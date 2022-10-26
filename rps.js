fd1434321a92753e9ded53f632c2ec5d3a597003
Leo Bruell <leobruell@gmail.com>
2022-10-26 14:25:57 -0400

    Make all buttons freeze

--- rps.js
+++ rps.js
@@ -31,55 +31,45 @@ function gamePlay(player, computer){
 
 
 
-buttons = document.querySelectorAll('button')
+let buttons = document.querySelectorAll('button')
+let wins = document.querySelector('.wins')
+let loss = document.querySelector('.losses')
+let tie = document.querySelector('.ties')
+let body = document.querySelector('body')
+let showResult = document.querySelector('.results p')
 
-buttons.forEach((button) => {
-    button.addEventListener('click', function makeGame(){
-        let result = gamePlay(button.textContent, computerPlay())
-    
-        let wins = document.querySelector('.wins')
-        let loss = document.querySelector('.losses')
-        let tie = document.querySelector('.ties')
-        let body = document.querySelector('body')
 
-        if (result.includes('Win')){
-            wins.textContent = Number(wins.textContent) + 1
-        }
-        else if (result.includes('Lose')) {
-            loss.textContent = Number(loss.textContent) + 1
-        }
-        else if (result.includes('Tie')){
-            tie.textContent = Number(tie.textContent) + 1
-        }
 
-        let showResult = document.createElement('p')
-        showResult.textContent = result
-        if (wins.textContent == 5){
-            showResult.textContent = "You won!!!"
-            showResult.style.cssText = "font-size: 24px; font-weight: bold;"
-            myResults.appendChild(showResult)
+buttons.forEach(button => {
+    button.addEventListener('click', makeGame)
+})
+
+function makeGame(event){
+    let result = gamePlay(event.target.textContent, computerPlay())
+    showResult.textContent = result
+    if (result.includes('Win')){
+        wins.textContent = Number(wins.textContent) + 1
+    }
+    else if (result.includes('Lose')) {
+        loss.textContent = Number(loss.textContent) + 1
+    }
+    else if (result.includes('Tie')){
+        tie.textContent = Number(tie.textContent) + 1
+    }
+    checkScore()
+}
+
+function checkScore(){
+    if (wins.textContent == 5){
+        showResult.textContent = "You won!!!"
+        showResult.style.cssText = "font-size: 24px; font-weight: bold;"
+        buttons.forEach(button => {
             button.removeEventListener('click', makeGame)
-            querySelector('.rock').removeEventListener('click', makeGame)
-            querySelector('.paper').removeEventListener('click', makeGame)
-
-        }
-        else if (loss.textContent == 5){
-            showResult.textContent = "You lost!!!"
-            showResult.style.cssText = "font-size: 24px; font-weight: bold;"
-            myResults.appendChild(showResult)
-            for (let button in buttons){
-                button.removeEventListener('click', makeGame)
-            }        
-        }
-
-        myResults = document.querySelector('.results')
-        if (myResults.childElementCount != 0){
-            myResults.removeChild(myResults.firstElementChild)
-        }
-        myResults.appendChild(showResult)
-    })    
-});
-
-
-
-
+        })}
+    else if (loss.textContent == 5){
+        showResult.textContent = "You lost!!!"
+        showResult.style.cssText = "font-size: 24px; font-weight: bold;"
+        buttons.forEach(button => {
+            button.removeEventListener('click', makeGame)
+        })}
+}
